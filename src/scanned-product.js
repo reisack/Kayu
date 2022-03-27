c1426879b03d0d24b5cde85f858570bff1d712d1
Rémi Eisack <remi.eisack@gmail.com>
2022-03-27 20:02:40 +0200

    Add calculation informations

--- src/scanned-product.js
+++ src/scanned-product.js
@@ -26,11 +26,58 @@ const ScannedProduct = ( {eanCode} ) => {
         return {
             frName: product.product_name_fr,
             brands: product.brands,
-            genericFrName: product.generic_name_fr,
-            nutriscoreGrade: product.nutriscore_grade,
             imageHeight: product.images.front_fr.sizes["200"].h,
             imageWidth: product.images.front_fr.sizes["200"].w,
             imageUrl: product.image_front_url,
+            scores: getScoresFromProduct(product)
+        };
+    }
+
+    const getScoresFromProduct = (product) => {
+        // Fat
+        const fat = product.nutriments["fat_100g"] ?? null;
+        const saturatedFat = product.nutriments["saturated-fat_100g"] ?? null;
+
+        let fatScore = null;
+        if (fat !== null) {
+            fatScore = fat * 0.75;
+        }
+        if (saturatedFat !== null) {
+            fatScore += saturatedFat * 0.25;
+        }
+        if (fatScore !== null) {
+            fatScore = fatScore.toFixed(2);
+        }
+
+        const sugarScore = product.nutriments["sugars_100g"] ?? null;
+        const saltScore = product.nutriments["salt_100g"] ?? null;
+
+        const novaGroup = product.nova_group ?? null;
+        let novaGroupScore = null;
+        if (novaGroup !== null) {
+            novaGroupScore = novaGroup * 25;
+        }
+
+        const eco = product.ecoscore_score ?? null;
+        let ecoScore;
+        if (eco) {
+            ecoScore = 100 - eco;
+        }
+        
+        const additives = product.additives_tags || null;
+        let additivesScore;
+        if (additives) {
+            // Todo : Get additives Informations
+            const additivesInformations = [];
+        }
+
+        return {
+            fat: fatScore,
+            sugar: sugarScore,
+            salt: saltScore,
+            novaGroup: novaGroupScore,
+            eco: ecoScore,
+            additives: additivesScore
         };
     }
 
@@ -43,8 +90,12 @@ const ScannedProduct = ( {eanCode} ) => {
             {isLoading ? <ActivityIndicator /> : (
                 <>
                     <Text>{data.frName} - {data.brands}</Text>
-                    <Text>{data.genericFrName}</Text>
-                    <Text>Nutriscore : {data.nutriscoreGrade}</Text>
+                    {data.scores.fat !== null && <Text>Gras : {data.scores.fat} g</Text>}
+                    {data.scores.sugar !== null && <Text>Sucre : {data.scores.sugar} g</Text>}
+                    {data.scores.salt !== null && <Text>Sel : {data.scores.salt} g</Text>}
+                    {data.scores.novaGroup !== null && <Text>NOVA : {data.scores.novaGroup}</Text>}
+                    {data.scores.eco !== null && <Text>Ecoscore : {data.scores.eco}</Text>}
+                    {data.scores.additives !== null && <Text>Addictifs : {data.scores.additives}</Text>}
                     <Image 
                         style={{width: data.imageWidth, height: data.imageHeight}}
                         source={{uri: data.imageUrl}}
