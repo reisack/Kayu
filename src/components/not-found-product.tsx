a3b216b1436b31d1adf5afd2fe3a8c39cec878ca
Rémi Eisack <remi.eisack@gmail.com>
2022-07-14 01:29:25 +0200

    Design not found product screen

--- src/components/not-found-product.tsx
+++ src/components/not-found-product.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {useTranslation} from 'react-i18next';
 
 const NotFoundProduct: React.FC = () => {
   const {t} = useTranslation();
 
+  const styles = StyleSheet.create({
+    message: {
+      paddingTop: 16,
+      paddingLeft: 16,
+      fontSize: 16,
+      textAlign: 'left',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
     <View>
-      <Text>{t<string>('notFoundProduct')}</Text>
+      <Text style={styles.message}>{t<string>('notFoundProduct')}</Text>
     </View>
   );
 };
