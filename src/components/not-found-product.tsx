ba867e91fcf2d7b4e77657f41ce97973d7b81935
Rémi Eisack <remi.eisack@gmail.com>
2022-07-17 18:52:02 +0200

    Design not found product page

--- src/components/not-found-product.tsx
+++ src/components/not-found-product.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import {useTranslation} from 'react-i18next';
 
 const NotFoundProduct: React.FC = () => {
@@ -9,15 +9,27 @@ const NotFoundProduct: React.FC = () => {
     message: {
       paddingTop: 16,
       paddingLeft: 16,
-      fontSize: 16,
+      fontSize: 24,
       textAlign: 'left',
       alignItems: 'center',
       justifyContent: 'center',
+      paddingHorizontal: 16,
+    },
+    // https://www.flaticon.com/free-icon/sad-face-in-rounded-square_42901
+    notFoundImage: {
+      width: 128,
+      height: 128,
+      alignSelf: 'center',
+      marginVertical: 32
     },
   });
 
   return (
     <View>
+      <Image
+        style={styles.notFoundImage}
+        source={require('../../assets/images/sad.png')}
+      />
       <Text style={styles.message}>{t<string>('notFoundProduct')}</Text>
     </View>
   );
