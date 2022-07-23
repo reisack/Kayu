c6cea8168562bc2f3c54db0ea84b117c114c4a98
Rémi Eisack <remi.eisack@gmail.com>
2022-07-23 22:17:12 +0200

    Make App Consts read only

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export default class Consts {
-  static openFoodFactAPIBaseUrl = 'https://fr.openfoodfacts.org/';
-  static httpHeaderGetRequest: any = {
+  static readonly openFoodFactAPIBaseUrl = 'https://fr.openfoodfacts.org/';
+  static readonly httpHeaderGetRequest: any = {
     method: 'GET',
     headers: {
       'User-Agent': 'Kayu - Android - Version 1.0',
