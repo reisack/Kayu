c132db2008825ae00ee845d8d51c219e3d083d5e
Rémi Eisack <remi.eisack@gmail.com>
2026-06-20 18:24:47 +0200

    ToastAndroid is only called when OS is Android

--- src/services/toast-service.ts
+++ src/services/toast-service.ts
@@ -1,13 +1,10 @@
 import { Platform, ToastAndroid } from 'react-native';
 
 export default class ToastService {
-  public static show(
-    message: string,
-    duration: number = ToastAndroid.LONG,
-  ): void {
+  public static show(message: string, duration?: number): void {
     // Kayu is Android-only today, so non-Android platforms intentionally no-op here.
     if (Platform.OS === 'android') {
-      ToastAndroid.show(message, duration);
+      ToastAndroid.show(message, duration || ToastAndroid.LONG);
     }
   }
 }
